54af213b9705a90a8c165af0b1dfc4ba6286fcf7
Ties de Kock <ties@tiesdekock.nl>
2020-04-11 17:41:25 +0200

    Add a first test

---
+++ tests/test_function_ids_are_unique.py
@@ -0,0 +1,20 @@
+from compal.functions import GetFunction, SetFunction
+
+def validate_key_value_object(key_value_object):
+    known_keys = set()
+    known_values = set()
+
+    for attr, value in vars(GetFunction).items():
+        assert attr not in known_keys
+        assert value not in known_values
+
+        known_keys.add(attr)
+        known_values.add(value)
+
+
+def test_get_functions_are_unique():
+    validate_key_value_object(GetFunction)
+
+
+def test_set_functions_are_unique():
+    validate_key_value_object(SetFunction)
